3ca63156509098e1774673e9ab2ea5cc04ca17bb
Muhsin-42 <muhsinofficial21@gmail.com>
2024-02-25 19:59:47 +0530

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Pdf Genie",
   description: "Pdf Genie - Your Gpt for the pdf.",
@@ -25,6 +25,7 @@ export default function RootLayout({
         <div className="absolute right-[11rem] top-[-6rem] -z-10 size-[31.25rem] translate-y-[-50%] rounded-full bg-[#f8a0a2] blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="absolute left-[-35rem] top-[-1rem] -z-10 size-[50rem] translate-y-[-50%] rounded-full bg-[#b6aef5] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]  xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         {children}
+        <Analytics />
       </body>
     </html>
   );
