36c50b37e42a96d3f38b793b8c3da53f0de9f4e8
Muhsin-42 <muhsinofficial21@gmail.com>
2024-02-27 00:05:11 +0530

    Modified pdf viewer

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  //   webpack: (config) => {
-  //     config.module.rules.push({
-  //       test: /\.node/,
-  //       use: "raw-loader",
-  //     });
-  //     return config;
-  //   },
-  webpack: (config) => {
-    config.externals = [...config.externals, "jsdom"];
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
